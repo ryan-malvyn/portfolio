7fdc90aa0c3dbf7adcb0cf72010aca3c8f88e905
Ryan Malvyn <99323305+ryan-malvyn@users.noreply.github.com>
2022-08-17 22:08:05 +0800

    bug fixed

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,7 +1,25 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const mail = require('@sendgrid/mail')
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
 
 export default async function handler(req, res) {
   const body = JSON.parse(req.body);
-  console.log(body)
+
+  const message = `
+  Name: ${body.name}\r\n
+  Email: ${body.email}\r\n
+  Message: ${body.message}
+  `;
+
+  const data= {
+    to: 'ryanmalvyn2202@gmail.com',
+    from: 'ryanmalvyn22@gmail.com',
+    subject: 'New Web Form Message!',
+    text: message,
+    html: message.replace(/\r\n/g, '<br>')
+  }
+
+  mail.send(data)
   res.status(200).json({ status:'ok' })
 }
